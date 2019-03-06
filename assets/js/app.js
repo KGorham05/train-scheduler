6906ac8c2948cba5f9d88e1ad45ff2693576c258
Kgor <kevin.gorham@gmail.com>
2019-03-05 20:03:52 -0800

    basic files connected with jQuery, Bootstrap, and Firebase

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+var config = {
+    apiKey: "AIzaSyBAmst3BSZr_a7p9fjUNkeZ3oBL02KiLcg",
+    authDomain: "train-scheduler-95522.firebaseapp.com",
+    databaseURL: "https://train-scheduler-95522.firebaseio.com",
+    projectId: "train-scheduler-95522",
+    storageBucket: "train-scheduler-95522.appspot.com",
+    messagingSenderId: "1073844518517"
+  };
+  firebase.initializeApp(config);
+
+  $("h1").text("hello from jQuery");
