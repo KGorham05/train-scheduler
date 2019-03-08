87b60e4b2bad87ef5b8c8b8c238ac9111d273034
Kgor <kevin.gorham@gmail.com>
2019-03-07 17:56:47 -0800

    final train styling

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# train-scheduler
-Using Firebase to create a train scheduler application
+# Train Scheduler
+
+## Firebase project
+
+The **purpose** of this project is to practice interacting with firebase and the moment.js API.
+This page can be used to calculate the time until a particular train arrives.  
+
+**Firebase** was used to practice interacting with a server side database, and **moment.js** was used to calculate train times using the current time.
+
+**Get started** by clicking adding a train of your own. Then reload the page to see the data persist! 
+
+The **github** for this project can be found here: [Github](https://github.com/KGorham05/train-scheduler)
+
+This project was created and is maintained by **Kevin Gorham**. 
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,19 @@
-// configure firebase
+// Configure firebase
 var config = {
-    apiKey: "AIzaSyBAmst3BSZr_a7p9fjUNkeZ3oBL02KiLcg",
-    authDomain: "train-scheduler-95522.firebaseapp.com",
-    databaseURL: "https://train-scheduler-95522.firebaseio.com",
-    projectId: "train-scheduler-95522",
-    storageBucket: "train-scheduler-95522.appspot.com",
-    messagingSenderId: "1073844518517"
+    apiKey:                 "AIzaSyBAmst3BSZr_a7p9fjUNkeZ3oBL02KiLcg",
+    authDomain:             "train-scheduler-95522.firebaseapp.com",
+    databaseURL:            "https://train-scheduler-95522.firebaseio.com",
+    projectId:              "train-scheduler-95522",
+    storageBucket:          "train-scheduler-95522.appspot.com",
+    messagingSenderId:      "1073844518517"
 };
+
 firebase.initializeApp(config);
-
-// Global variable list
+// Save the firebase database to a variable
 var database = firebase.database();
-// Display intial variables on the page in the table
 
+// function for creating, calculating, and displaying train information on the page. 
+// takes in a snapshot from firebase as a variable. 
 function displayTrainInfo(snapshot) {
  
     var tr                  = $("<tr>");
@@ -35,17 +36,8 @@ function displayTrainInfo(snapshot) {
     tdTrain                 .text(snapshot.val().name);
     tdDestination           .text(snapshot.val().dest);
     tdFrequency             .text(snapshot.val().freq);
-    tdNextArrival           .text(nextTrain.format("HH:mm a"));
+    tdNextArrival           .text(nextTrain.format("h:mm a"));
     tdMinAway               .text(tMinutesTillTrain);
-   
-    console.log("val().freq =" + snapshot.val().freq)
-    console.log("ftt =" + ftt)
-    console.log("fttConverted =" + fttConverted);
-    console.log("current time =" + currentTime);
-    console.log("diffTime = " + diffTime)
-    console.log("tRemainder =" + tRemainder)
-    console.log("tMinutesTillTrain =" + tMinutesTillTrain)
-    console.log("nextTrain =" + nextTrain)
 
     tr.append(tdTrain);
     tr.append(tdDestination);
@@ -55,36 +47,35 @@ function displayTrainInfo(snapshot) {
     tr.appendTo($("#train-list"));
 };
 
-
 // Listen for the click on the submit button
 $(document).on("click", "#add-train-btn", function () {
 
     // capture the text in the form fields and save it as a variable
 
-    var trainName = $("#train-name-input").val().trim();
-    var destination = $("#destination-input").val().trim();
-    var firstTrainTime = $("#first-train-time-input").val().trim();
-    var frequency = $("#frequency-input").val().trim();
+    var trainName =         $("#train-name-input").val().trim();
+    var destination =       $("#destination-input").val().trim();
+    var firstTrainTime =    $("#first-train-time-input").val().trim();
+    var frequency =         $("#frequency-input").val().trim();
 
     // getElementById returns an element object that I can use to interact with the element. 
     // I'm saving each field as an object so I can make sure all fields have input before adding a train to firebase.
 
-    var trainNameObject = document.getElementById("train-name-input");
-    var destObject = document.getElementById("destination-input");
-    var fttObject = document.getElementById("first-train-time-input");
-    var freqObject = document.getElementById("frequency-input");
+    var trainNameObject =   document.getElementById("train-name-input");
+    var destObject =        document.getElementById("destination-input");
+    var fttObject =         document.getElementById("first-train-time-input");
+    var freqObject =        document.getElementById("frequency-input");
 
-    // I don't entirely understand how this section works 
+    // There's got to be a better way of checking this... 
     if (
-        (trainNameObject && trainNameObject.value) &&
-        (destObject && destObject.value) &&
-        (fttObject && fttObject.value) &&
-        (freqObject && freqObject.value)
+        (trainNameObject   && trainNameObject.value) &&
+        (destObject        && destObject.value) &&
+        (fttObject         && fttObject.value) &&
+        (freqObject        && freqObject.value)
     ) {
         database.ref().push({
             name: trainName,
             dest: destination,
-            ftt: firstTrainTime,
+            ftt:  firstTrainTime,
             freq: frequency
         });
     } else {
@@ -93,10 +84,9 @@ $(document).on("click", "#add-train-btn", function () {
     document.getElementById("train-input-form").reset();
 
 });
-
+// This runs the displayTrainInfo function when the page loads, and if another train is added to the database.
 database.ref().on("child_added", function (childSnap) {
     displayTrainInfo(childSnap);
-
 });
 
 
